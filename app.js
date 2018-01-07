8f8c9c915283887d0fdf0135fdbc29cee3700a9c
zicong <zicong@igola.com>
2018-01-07 15:34:25 +0800

    Push 与微信号绑定

--- app.js
+++ app.js
@@ -5,7 +5,26 @@ const config = require('./config')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello Node.js')
+
+  var signature = req.query.signature
+  var timestamp = req.query.timestamp
+  var nonce = req.query.nonce
+  var echostr = req.query.echostr
+
+  var array = [config.token, timestamp, nonce]
+  array.sort()
+
+  var tempStr = array.join('')
+  const hashCode = crypto.createHash('sha1')
+  var resultCode = hashCode.update(tempStr, 'utf8').digest('hex')
+
+  if (resultCode === signature) {
+    res.send(echostr)
+  } else {
+    res.send('mismatch')
+  }
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('server is running')
+})
